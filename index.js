aca5a7a57d081c455a874a3b864637bad9cf8a03
Michael Franco <franco.luis.michael@gmail.com>
2022-03-17 11:51:44 -0500

    commit tree 14292b515c7ff30be25f31be8e4567ffdb1458d7

---
+++ index.js
@@ -0,0 +1,19 @@
+// const divide = (num1, num2) => num1 / num2;
+
+let num1 = 2000;
+let num2 = 100;
+const divide = function() {
+    return num1 / num2;
+}
+
+let x = 2;
+const square = (x) => x * x; {
+
+}
+
+let y = 4;
+let z = 3;
+const add = (y, z) => y + z; {}
+
+
+
